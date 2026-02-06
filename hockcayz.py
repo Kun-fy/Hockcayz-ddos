1b1f66fc692dcfb3fe717680129e6380ab2860c2
Kun-fy <kosakatakun@gmail.com>
2026-02-06 16:53:58 +0700

    Update hockcayz.py

--- hockcayz.py
+++ hockcayz.py
@@ -309,7 +309,7 @@ if __name__ == '__main__':
                             i.join()
 
                     logging.info(
-                        'Respon Req: {0} - 'Fore.WHITE + 'Nuked Req: {1} - 'Fore.CYAN + 'Bot: {2} - 'Fore.YELLOW + 'Time: {3} seconds.'.format(str(num_success), str(num_failed), str(num_bot_requests), round(elapsed_time)), end='\r', flush=True)
+                        'Responded Requests: {0} - Nuked Requests: {1} - Bot Requests: {2} - Elapsed Time: {3} seconds.'.format(
                             str(num_success),
                             str(num_failed),
                             str(num_bot_requests),
